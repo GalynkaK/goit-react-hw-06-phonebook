82f9b3c1717df362fba53378320571c84a3b688d
GalynkaK <gkarpinskaya@gmail.com>
2023-06-12 10:22:57 +0300

    rewrittten redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,81 @@
-export const App = () => {
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Form from "./Form";
+import ContactList from "./ContactList";
+import Filter from "./Filter";
+import { addContact, delContact } from "redux/contacts/actions";
+import { getContacts, getFilter } from "redux/contacts/contacts-selector";
+
+const App = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const dispatch = useDispatch();
+
+  const [firstRenderFlag, setFlag] = useState(true);
+
+  useEffect(() => { }, []);
+
+  useEffect(() => {
+    if (firstRenderFlag) {
+      const contactsFromLocalStorage = localStorage.getItem('contactList');
+
+      if (contactsFromLocalStorage !== 'undefined') {
+        const parsedContacts = JSON.parse(contactsFromLocalStorage);
+
+        if (parsedContacts) {
+        }
+      }
+      setFlag(false);
+    } else {
+      localStorage.setItem('contactList', JSON.stringify(contacts));
+    }
+  }, [contacts, firstRenderFlag]);
+
+  const handleSubmit = e => {
+    const name = e.name;
+    const number = e.number;
+    const contactsLists = [...contacts];
+
+    if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      dispatch(addContact(name, number));
+    }
+  };
+
+  const handleDelete = e => {
+    dispatch(delContact(e));
+  };
+
+  const getFilteredContacts = () => {
+    const filterContactsList = contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
+    return filterContactsList;
+  };
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <Form handleSubmit={handleSubmit} />
+      <h2> Contacts</h2>
+      <Filter />
+      <ContactList
+        contacts={getFilteredContacts()}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import css from "./Form.module.css";
+
+export const Form = ({ handleSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChangeName = e => {
+    const { value } = e.target;
+    setName(value);
+  };
+
+  const handleChangeNumber = e => {
+    const { value } = e.target;
+    setNumber(value);
+  };
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    handleSubmit({ name: name, number: number });
+    form.reset();
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleFormSubmit}>
+      <label className={css.label}>Name </label>
+      <input
+        className={css.input}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        placeholder="Enter name"
+        value={name}
+        onChange={handleChangeName}
+      />
+      <label className={css.label}>Number </label>
+      <input
+        className={css.input}
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        placeholder="Enter phone number"
+        value={number}
+        onChange={handleChangeNumber}
+      />
+      <button className={css.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
+
+Form.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default Form;
